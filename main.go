242b264f2eaa1a70028809eaf666084fc18b8f60
pyslas <pabloislas@gmail.com>
2022-04-22 17:31:48 -0400

    File Server

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
-func handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
-}
+// func handler(writer http.ResponseWriter, request *http.Request) {
+// 	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+// }
+
+/*func index(writer http.ResponseWriter, request *http.Request) {
+	t, _ := template.ParseFiles("templates/index.html")
+	t.Execute(writer, "Hello World!")
+}*/
+
 func main() {
-	http.HandleFunc("/", handler)
+	fs := http.FileServer(http.Dir("./templates"))
+	http.Handle("/", fs)
 	http.ListenAndServe(":8081", nil)
 }

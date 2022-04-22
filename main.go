9b8a7570f5a6f81eb40fcd741838ab2545749b72
pyslas <pabloislas@gmail.com>
2022-04-22 16:28:58 -0400

    Hello World

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func handler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+}
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8081", nil)
 }

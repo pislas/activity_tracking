33bfd6c0f0db4d11c3f8496fc8139eb46541272f
pyslas <pabloislas@gmail.com>
2022-05-20 16:01:58 -0400

    FindUser

--- users/user.go
+++ users/user.go
@@ -1,5 +1,9 @@
 package users
 
+import (
+	"fmt"
+)
+
 var UsersDb *Users
 
 type User struct {
@@ -20,4 +24,13 @@ func (s *Users) Insert(u User) User {
 	u.Id = uint64(len(s.users))
 	s.users = append(s.users, u)
 	return u
+}
+
+func (s *Users) FindUser(username string) (*User, error) {
+	for _, u := range s.users {
+		if u.Username == username {
+			return &u, nil
+		}
+	}
+	return nil, fmt.Errorf("usuario %s invalido", username)
 }
